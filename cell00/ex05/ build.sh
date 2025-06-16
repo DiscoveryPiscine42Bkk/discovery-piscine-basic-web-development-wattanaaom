a6cdc193ead46107fd5aa6eb8233d77fad417817
wattanaaom <wattanapong1810@gmail.com>
2025-06-16 04:52:45 +0000

    05

--- cell00/ex05/ build.sh
+++ cell00/ex05/ build.sh
@@ -5,5 +5,5 @@ for arg in "$@"
 do
     dir="ex$arg"
     mkdir -p "$dir"
-    echo "drwxr-xr-x 1  will user40 4, Ok manew 14 22:25 $dir"
+    echo "drwxr-xr-x 1  will user40 4, Ok wattanaaom 13 11:43 $dir"
 done
